987c120a4d8915884f6db18797ffc8c7e2bbe2ec
Kon <discipline31shogi@gmail.com>
2024-04-18 21:21:59 +0900

    add debugFunction for debugging

---
+++ src/debug-function.test.ts
@@ -0,0 +1,8 @@
+import { debugFunction } from './debug-function'
+
+// This test file is just for debugging functions
+describe('execute debugFunction', () => {
+  it('start debug', () => {
+    debugFunction()
+  })
+})
---
+++ src/debug-function.ts
@@ -0,0 +1,6 @@
+// This function is for debugging execution.
+// Write the code you want to debug and run the test file.
+export function debugFunction() {
+  debugger
+  // Write the code you want to debug here
+}
